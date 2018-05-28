7531d38ecea917ff1d5ac5d515efb252472a3004
Shubhamsharma09 <sann891997@gmail.com>
2018-05-28 16:44:45 +0530

    Data Collection ---

---
+++ Jddesc/jd1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+lis=['python', 'java' ,'nodejs']
+z=0
+
+class Jd1Spider(scrapy.Spider):
+    name = "jd1"
+    #allowed_domains = ["toptal.com"]
+    urls ='https://www.toptal.com/{}/job-description'
+    start_urls=[urls.format(lis[z])]
+
+    def parse(self, response):
+        global lis,z
+        self.log('Scrapping started' + response.url)
+        a=response.css("h3 ~ p::text").extract()
+        Job_title= lis[z]
+        Job_overview= str(a[0])
+        Responsibilities = []
+        Qualifications=[]
+        b=response.css("h3 ~ ul")
+        b=b[:2]
+        for i in b[:1]:
+           c= (i.css("li::text").extract())
+           for j in c:
+            Responsibilities.append(str(j))
+        
+        for i in b[1:]:
+            c= (i.css("li::text").extract())
+            for j in c:
+             Qualifications.append(str(j))
+        
+        items={
+         "Title":Job_title,
+         "Job_overview":Job_overview,
+         "Responsibilities":Responsibilities,
+         "Qualifications": Qualifications
+             }
+        with open('Jddesc.json', 'r+') as f:
+            f1 =json.load(f)
+            print(type(f1))
+        with open('Jddesc.json', 'r+') as f:
+             f1.append(items)
+             json.dump(f1,f)
+        z=z+1
+        if(z<3):
+           yield scrapy.Request(url =self.urls.format(lis[z]), callback= self.parse )
